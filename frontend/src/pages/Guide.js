8039d8a6014a0c737fede1dd85eaab136bfde18f
axeld <axel.dovskog@outlook.com>
2025-01-29 14:41:40 +0100

    Better guide functionality

--- frontend/src/pages/Guide.js
+++ frontend/src/pages/Guide.js
@@ -5,15 +5,20 @@ import DownloadCode from "../components/DownloadCode"
 // The default page. Login not required.
 function Guide() {
     
-    const [currentInstructions, setCurrentInstructions] = useState("area")
+    const [currentInstructions, setCurrentInstructions] = useState("classification")
     const [downloadFramework1, setDownloadFramework1] = useState("tensorflow")
     const [downloadFramework2, setDownloadFramework2] = useState("tensorflow")
 
     const [areaImageIsDark, setAreaImageIsDark] = useState(false)
+    const [classificationImageIsDark, setClassificationImageIsDark] = useState(false)
 
     return (
         <div className="guide-container">
             <div className="guide-toolbar">
+                <div className={"guide-toolbar-element " + (currentInstructions == "classification" ? "guide-toolbar-element-selected": "")}
+                onClick={() => setCurrentInstructions("classification")}>
+                    Classification Labeling
+                </div>
                 <div className={"guide-toolbar-element " + (currentInstructions == "area" ? "guide-toolbar-element-selected": "")}
                 onClick={() => setCurrentInstructions("area")}>
                     Area Labeling
@@ -25,14 +30,16 @@ function Guide() {
                 
             </div>
 
-            <div className="guide-main">
-                {currentInstructions == "download" && <div className="instructions-header">
+            {/* LOADING DATASETS */}
+            {currentInstructions == "download" && <div className="guide-main">
+
+                <div className="instructions-header">
                     <h1 className="instructions-title">Download Help</h1>
                     <p className="instructions-text">Downloaded datasets can easily be loaded into different machine learning frameworks. See below for examples for Tensorflow and Pytorch.
                         Note that the code provided applies to image datasets, and that the method used (folders as labels or filenames as labels) must be taken into account.
                     </p>
-                </div>}
-                {currentInstructions == "download" && <div className="instructions-container">
+                </div>
+                <div className="instructions-container">
                     <div className="download-instructions-element">
                         <h1 className="download-instructions-title">Folders as labels</h1>
 
@@ -56,16 +63,20 @@ function Guide() {
 
                         <DownloadCode name="YOUR_DATASET" datatype="classification" framework={downloadFramework2} downloadType="files"/>
                     </div>
-                </div>}
+                </div>
+            </div>}
 
-                {currentInstructions == "area" && <div className="instructions-header">
+            {/* AREA LABELLING */}
+            {currentInstructions == "area" && <div className="guide-main">
+                <div className="instructions-header">
                     <h1 className="instructions-title">Area Labelling</h1>
                     <p className="instructions-text">
                         Area labels can be used to highlight sections of images, identifying them as the label used, e.g. a car.
                         See the image below for more information.
                     </p>
-                </div>}
-                {currentInstructions == "area" && <div className="instructions-container">
+                </div>
+
+                <div className="instructions-container">
                     <div className="instructions-area-container" onMouseEnter={() => setAreaImageIsDark(true)} onMouseLeave={() => setAreaImageIsDark(false)}>
                         <img className={"instructions-area-image " + (areaImageIsDark ? "instructions-area-image-disabled" : "")} src={window.location.origin + "/static/images/examplePage.jpg"} />
                         
@@ -87,8 +98,40 @@ function Guide() {
                             Clicking on a point allows you to move or delete it.
                         </div>
                     </div>
-                </div>}
-            </div>
+                </div>
+            </div>}
+
+
+            {/* CLASSIFICATION LABELLING */}
+            {currentInstructions == "classification" && <div className="guide-main">
+                <div className="instructions-header">
+                    <h1 className="instructions-title">Classification Labelling</h1>
+                    <p className="instructions-text">
+                        Classification labels are used to classify elements, identifying them as the label used, e.g. as an image of a specific car brand.
+                        See the image below for more information.
+                    </p>
+                </div>
+
+                <div className="instructions-container">
+                    <div className="instructions-area-container" onMouseEnter={() => setClassificationImageIsDark(true)} onMouseLeave={() => setClassificationImageIsDark(false)}>
+                        <img className={"instructions-area-image " + (classificationImageIsDark ? "instructions-area-image-disabled" : "")} 
+                        src={window.location.origin + "/static/images/exampleClassification.jpg"} />
+                        
+                        <div className="instructions-area-comment" style={{top: "31%", right: "1%"}}>
+                            Added labels are listed here, along with their keybind. Clicking the label or its keybind applies the label.
+                        </div>
+
+                        <div className="instructions-area-comment" style={{top: "53%", left: "1%"}}>
+                            If an element has been labelled, the color of its label will be shown next to its name.
+                        </div>
+
+                        <div className="instructions-area-comment" style={{top: "2%", right: "17%"}}>
+                            When applying a label, its color will briefly be shown in this circle.
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>}
         </div>
     )
 }
