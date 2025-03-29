5dd9b988def04f8a4537ae68dd7f7cde36d65261
axeld <axel.dovskog@outlook.com>
2025-03-29 19:13:52 +0100

    Working on improved guide

--- frontend/src/pages/Guide.js
+++ frontend/src/pages/Guide.js
@@ -5,7 +5,7 @@ import DownloadCode from "../components/DownloadCode"
 // The default page. Login not required.
 function Guide({BACKEND_URL}) {
     
-    const [currentInstructions, setCurrentInstructions] = useState("classification")
+    const [currentInstructions, setCurrentInstructions] = useState("datasets")
     const [downloadFramework1, setDownloadFramework1] = useState("tensorflow")
     const [downloadFramework2, setDownloadFramework2] = useState("tensorflow")
 
@@ -17,30 +17,50 @@ function Guide({BACKEND_URL}) {
     return (
         <div className="guide-container">
             <div className="guide-toolbar">
-                <div className={"guide-toolbar-element " + (currentInstructions == "classification" ? "guide-toolbar-element-selected": "")}
-                onClick={() => setCurrentInstructions("classification")}>
-                    Classification Labeling
+                <div className={"guide-toolbar-element " + (currentInstructions == "datasets" ? "guide-toolbar-element-selected": "")}
+                onClick={() => setCurrentInstructions("datasets")}>
+                    Datasets
                 </div>
-                <div className={"guide-toolbar-element " + (currentInstructions == "area" ? "guide-toolbar-element-selected": "")}
-                onClick={() => setCurrentInstructions("area")}>
-                    Area Labeling
+                    <div className={"guide-toolbar-element guide-toolbar-subelement " + (currentInstructions == "dataset-classification" ? "guide-toolbar-element-selected": "")}
+                    onClick={() => setCurrentInstructions("dataset-classification")}>
+                        Classification
+                    </div>
+                    <div className={"guide-toolbar-element guide-toolbar-subelement " + (currentInstructions == "dataset-area" ? "guide-toolbar-element-selected": "")}
+                    onClick={() => setCurrentInstructions("dataset-area")}>
+                        Area
+                    </div>
+                    <div className={"guide-toolbar-element guide-toolbar-subelement " + (currentInstructions == "datasets-loading" ? "guide-toolbar-element-selected": "")}
+                    onClick={() => setCurrentInstructions("datasets-loading")}>
+                        Loading Datasets
+                    </div>
+                <div className={"guide-toolbar-element " + (currentInstructions == "models" ? "guide-toolbar-element-selected": "")}
+                onClick={() => setCurrentInstructions("models")}>
+                    Models
                 </div>
-                <div className={"guide-toolbar-element " + (currentInstructions == "model" ? "guide-toolbar-element-selected": "")}
-                onClick={() => setCurrentInstructions("model")}>
-                    Model Creation
-                </div>
-                <div className={"guide-toolbar-element " + (currentInstructions == "download" ? "guide-toolbar-element-selected": "")}
-                onClick={() => setCurrentInstructions("download")}>
-                    Loading Datasets
-                </div>
+                    <div className={"guide-toolbar-element guide-toolbar-subelement " + (currentInstructions == "model-layers" ? "guide-toolbar-element-selected": "")}
+                    onClick={() => setCurrentInstructions("model-layers")}>
+                        Layers
+                    </div>
+                    <div className={"guide-toolbar-element guide-toolbar-subelement " + (currentInstructions == "model-building" ? "guide-toolbar-element-selected": "")}
+                    onClick={() => setCurrentInstructions("model-building")}>
+                        Building
+                    </div>
+                    <div className={"guide-toolbar-element guide-toolbar-subelement " + (currentInstructions == "model-compiling" ? "guide-toolbar-element-selected": "")}
+                    onClick={() => setCurrentInstructions("model-compiling")}>
+                        Compiling
+                    </div>
+                    <div className={"guide-toolbar-element guide-toolbar-subelement " + (currentInstructions == "model-training" ? "guide-toolbar-element-selected": "")}
+                    onClick={() => setCurrentInstructions("model-training")}>
+                        Training
+                    </div>
                 
             </div>
 
             {/* LOADING DATASETS */}
-            {currentInstructions == "download" && <div className="guide-main">
+            {currentInstructions == "datasets-loading" && <div className="guide-main">
 
                 <div className="instructions-header">
-                    <h1 className="instructions-title">Download Help</h1>
+                    <h1 className="instructions-title">Loading Datasets</h1>
                     <p className="instructions-text">Downloaded datasets can easily be loaded into different machine learning frameworks. See below for examples for TensorFlow and Pytorch.
                         Note that the code provided applies to image datasets, and that the method used (folders as labels or filenames as labels) must be taken into account.
                     </p>
@@ -84,119 +104,6 @@ function Guide({BACKEND_URL}) {
                 </div>
             </div>}
 
-            {/* AREA LABELLING */}
-            {currentInstructions == "area" && <div className="guide-main">
-                <div className="instructions-header">
-                    <h1 className="instructions-title">Area Labelling</h1>
-                    <p className="instructions-text">
-                        Area labels can be used to highlight sections of images, identifying them as the label used, e.g. a car.
-                        See the image below for more information.
-                    </p>
-                </div>
-
-                <div className="instructions-container">
-                    <div className="instructions-area-container" 
-                    onMouseEnter={() => setAreaImageIsDark(true)} 
-                    onMouseLeave={() => setAreaImageIsDark(false)}
-                    style={{display: (imageIsLoaded ? "flex" : "none")}}>
-                        <img className={"instructions-area-image " + (areaImageIsDark ? "instructions-area-image-disabled" : "")}
-                        src={BACKEND_URL + "/static/images/examplePage.jpg"} 
-                        onLoad={() => setImageIsLoaded(true)}/>
-                        
-                        <div className="instructions-area-comment" style={{top: "14%", right: "14%"}}>
-                            Clicking on a label or applying its keybind selects it.
-                        </div>
-                        
-                        <div className="instructions-area-comment" style={{top: "65%", right: "1%"}}>
-                            Areas added to the current element are listed here.
-                            The number of points is shown to the right. Clicking on an area selects it.
-                        </div>
-
-                        <div className="instructions-area-comment" style={{top: "79%", right: "25%"}}>
-                            Areas consist of an arbitrary number of points. 
-                            Clicking on the image with a label or area selected adds a point to that area. Dragging will create a square of points.
-                        </div>
-
-                        <div className="instructions-area-comment" style={{top: "66%", right: "67%"}}>
-                            Clicking on a point allows you to move or delete it.
-                        </div>
-                    </div>
-                </div>
-            </div>}
-
-
-            {/* CLASSIFICATION LABELLING */}
-            {currentInstructions == "classification" && <div className="guide-main">
-                <div className="instructions-header">
-                    <h1 className="instructions-title">Classification Labelling</h1>
-                    <p className="instructions-text">
-                        Classification labels are used to classify elements, identifying them as the label used, e.g. as an image of a specific car brand.
-                        See the image below for more information.
-                    </p>
-                </div>
-
-                <div className="instructions-container">
-                    <div className="instructions-area-container" 
-                    onMouseEnter={() => setClassificationImageIsDark(true)} 
-                    onMouseLeave={() => setClassificationImageIsDark(false)}
-                    style={{display: (imageIsLoaded ? "flex" : "none")}}>
-                        <img className={"instructions-area-image " + (classificationImageIsDark ? "instructions-area-image-disabled" : "")} 
-                        src={BACKEND_URL + "/static/images/exampleClassification.jpg"} 
-                        onLoad={() => setImageIsLoaded(true)}/>
-                        
-                        <div className="instructions-area-comment" style={{top: "49%", right: "1%"}}>
-                            Added labels are listed here, along with their keybind. Clicking the label or its keybind applies the label.
-                        </div>
-
-                        <div className="instructions-area-comment" style={{top: "43%", left: "1%"}}>
-                            If an element has been labelled, the color of its label will be shown next to its name.
-                        </div>
-
-                        <div className="instructions-area-comment" style={{top: "6%", right: "15%"}}>
-                            When applying a label, its color will briefly be shown in this circle. The image can also be resized here.
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>}
-
-            {/* MODEL CREATION */}
-            {currentInstructions == "model" && <div className="guide-main">
-                <div className="instructions-header">
-                    <h1 className="instructions-title">Model Creation</h1>
-                    <p className="instructions-text">
-                        Machine-learning models can be created in Django by gradually adding different types of layers. Once the model is completed, it can be built and then downloaded.
-                        See the image below for more information.
-                    </p>
-                </div>
-
-                <div className="instructions-container">
-                    <div className="instructions-area-container" 
-                    onMouseEnter={() => setAreaImageIsDark(true)} 
-                    onMouseLeave={() => setAreaImageIsDark(false)}
-                    style={{display: (imageIsLoaded ? "flex" : "none")}}>
-                        <img className={"instructions-area-image " + (areaImageIsDark ? "instructions-area-image-disabled" : "")} 
-                        src={BACKEND_URL + "/static/images/examplePageModel.jpg"} 
-                        onLoad={() => setImageIsLoaded(true)}/>
-
-                        <div className="instructions-area-comment" style={{top: "10%", left: "13%"}}>
-                            Layers are listed in the sidebar, as well as in the main view to the right. They can be reordered by dragging them.
-                        </div>
-
-                        <div className="instructions-area-comment" style={{top: "20%", right: "9%"}}>
-                            Warnings will appear above layers in case of invalid layer combinations, such as a Conv2D after a Dense layer.
-                        </div>
-
-                        <div className="instructions-area-comment" style={{top: "6%", left: "36%"}}>
-                            Once the model is finished, it can be built (compiled) here. After being built it can be trained on datasets here in Dalinar or downloaded.
-                        </div>
-
-                        <div className="instructions-area-comment" style={{bottom: "13%", right: "17%"}}>
-                            Already created layers can be updated by changing their fields and saving changes.
-                        </div>
-                    </div>
-                </div>
-            </div>}
         </div>
     )
 }
