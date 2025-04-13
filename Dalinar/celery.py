45331a840aa6c9b1d5ef4491b4f728700f7c80ec
Axel Dovskog <ax0825do-s@student.lu.se>
2025-04-13 17:17:18 +0200

    Changes to celery

--- Dalinar/celery.py
+++ Dalinar/celery.py
@@ -2,11 +2,23 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
+from kombu import Queue
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dalinar.settings')
 
 app = Celery('Dalinar')
 app.config_from_object(settings, namespace='CELERY')
+
+app.conf.task_queues = (
+    Queue('default'),
+    Queue('training'),
+)
+
+app.conf.task_routes = {
+    'api.tasks.train_model_task': {'queue': 'training'},
+    'api.tasks.train_model_tensorflow_dataset_task': {'queue': 'training'},
+}
+
 app.autodiscover_tasks()
 
 @app.task(bind=True)
