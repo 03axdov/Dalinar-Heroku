5aa13dceafb9c719c3d36afaea532702809f424a
axeld <axel.dovskog@outlook.com>
2025-05-24 09:14:05 +0200

    Sitemap update

--- frontend/sitemaps.py
+++ frontend/sitemaps.py
@@ -1,5 +1,27 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from .models import Dataset, Model  # adjust path as needed
+
+class DatasetSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return Dataset.objects.filter(visibility='public')  # only index public datasets
+
+    def location(self, obj):
+        return f"/datasets/public/{obj.id}/"
+
+class ModelSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.7
+
+    def items(self):
+        return Model.objects.filter(visibility="public")
+
+    def location(self, obj):
+        return f"/models/public/{obj.id}/"
+    
 
 class StaticViewSitemap(Sitemap):
     def items(self):
